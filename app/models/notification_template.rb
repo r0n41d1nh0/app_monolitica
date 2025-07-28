b868218f16f67785416cde0a364346a375489788
Ronald Gomez <vaibanezronald@gmail.com>
2025-07-27 21:50:35 -0500

    Se adicionan los modelos notification_template y notification_request

---
+++ app/models/notification_template.rb
@@ -0,0 +1,6 @@
+class NotificationTemplate < ApplicationRecord
+    has_many :notification_requests, foreign_key: :notification_template_key, primary_key: :key
+
+    validates :key, presence: true, uniqueness: true
+
+end
