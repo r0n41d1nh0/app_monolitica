1b3c3b024671a637f2ef1a0656eb07c328c22c63
Ronald Gomez <vaibanezronald@gmail.com>
2025-07-31 14:05:21 -0500

    Se adiciona sidekiq en el proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  mount Sidekiq::Web => "/sidekiq"
 end
