7b70e9937fe936cf0f5ea2f1e638f465829c50f2
Ronald Gomez <vaibanezronald@gmail.com>
2025-08-01 00:49:47 -0500

    Corrección enum en modelo NotificationRequest

--- app/models/notification_request.rb
+++ app/models/notification_request.rb
@@ -1,12 +1,6 @@
 class NotificationRequest < ApplicationRecord
     belongs_to :notification_template
-
-    enum status: {
-        queued: "queued",
-        processing: "processing",
-        sent: "sent",
-        failed: "failed"
-    }
+    enum :status, queued: "queued", processing: "processing", sent: "sent", failed: "failed"
 
     validates :recipient, presence: true
     validates :channel, presence: true
