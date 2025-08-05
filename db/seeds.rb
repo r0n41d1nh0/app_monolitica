71193f933a05c2d51682f1f55cb59394f24ca657
Ronald Gomez <vaibanezronald@gmail.com>
2025-08-04 19:22:28 -0500

    Se adiciona seed para template por default

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Creando plantillas de notificación..."
+NotificationTemplate.find_or_create_by!(key: 'foo_notification') do |t|
+  t.default_title = 'Título de la notificación de prueba'
+  t.default_body = 'Este es el contenido de la notificación de prueba.'
+end
+puts "Plantillas creadas."
